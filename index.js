32609dd10134169e49607d32c3241d0ebb6bbc29
nathantmckenzie <nathantaylormckenzie@hotmail.com>
2025-11-27 18:32:10 -0800

    what

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("WHAT THE HECKKK");
 });
 
 app.post("/generate-pdf", async (req, res) => {
